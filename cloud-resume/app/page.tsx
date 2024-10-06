628bcc0ed3311e32d4dee7aa4b4969024b3a515a
Arnold De La Vega <arnoldd@device-0004.local>
2024-10-05 18:42:59 -0700

    Fixed Desktop view with 2 column view. However, mobile view is messed up here.

--- cloud-resume/app/page.tsx
+++ cloud-resume/app/page.tsx
@@ -9,8 +9,8 @@ import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
+    <section className="w-full flex flex-col items-center justify-center gap-4 py-8 border border-red-600 md:py-10">
+      <div className="w-full inline-block text-center justify-center">
         <span className={title()}>Hello, I'm Arnold,&nbsp;</span>
         <br />
         <span className={title()}>And this page has been viewed </span>
@@ -19,7 +19,7 @@ export default function Home() {
         <span className={title()}>times.</span>
       </div>
 
-      <div className="flex gap-3 mt-5">
+      <div className="w-full flex gap-3 mt-5 items-center justify-center">
         <Link
           isExternal
           className={buttonStyles({ variant: "bordered", radius: "full" })}
@@ -29,30 +29,33 @@ export default function Home() {
           GitHub
         </Link>
       </div>
-
-      <div className="items-center text-center mt-5">
-        <h2 className={`${title({ color: "violet" })}`}>Skills</h2>
-        <span className={subtitle()}>
-          <ul className="list-disc text-left">
-            <li>GCCH Administration</li>
-            <li>Azure</li>
-            <li>Intune</li>
-            <li>NIST 800-171</li>
-            <li>IaC</li>
-            <li>Bash</li>
-          </ul>
-        </span>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 items-start">
         <div className="items-center text-center mt-5">
-          <h2 className={`${title({ color: "violet" })}`}>Certifications</h2>
+          <h2 className={`${title({ color: "violet" })}`}>Skills</h2>
           <span className={subtitle()}>
-            <ul className="list-disc text-left">
-              <li>CompTIA Network+</li>
-              <li>CompTIA Security+</li>
-              <li>AZ-104</li>
-              <li>AZ-305</li>
-              <li>SC-300</li>
+            <ul className="list-disc pl-5 text-left md:mx-40">
+              <li>GCCH Administration</li>
+              <li>Azure</li>
+              <li>Intune</li>
+              <li>NIST 800-171</li>
+              <li>IaC</li>
+              <li>Bash</li>
             </ul>
           </span>
+        </div>
+        <div>
+          <div className="items-center text-center mt-5">
+            <h2 className={`${title({ color: "violet" })}`}>Certs</h2>
+            <span className={subtitle()}>
+              <ul className="list-disc pl-5 mx-auto md:mx-40 text-left md:text-left">
+                <li>CompTIA Network+</li>
+                <li>CompTIA Security+</li>
+                <li>AZ-104</li>
+                <li>AZ-305</li>
+                <li>SC-300</li>
+              </ul>
+            </span>
+          </div>
         </div>
       </div>
     </section>
