c85d625de9db809e8bf221d23315ba0cae71a24c
Arnold De La Vega <arnoldd@device-0004.local>
2024-10-13 00:58:36 -0700

    removed metadata favicon from /app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,6 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export const viewport: Viewport = {
@@ -69,8 +66,8 @@ export default function RootLayout({
                 href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
                 title="nextui.org homepage"
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
+                <span className="text-default-600">Made by </span>
+                <p className="text-primary">Arnold</p>
               </Link>
             </footer>
           </div>
