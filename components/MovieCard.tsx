bab481459b07b4efd20e13c223534cd2067cf66c
Arnold De La Vega <arnoldd@device-0004.local>
2024-10-13 18:04:31 -0700

    Fixed movie card image positioning

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -7,9 +7,15 @@ interface MovieCardProps {
   imgURL: string;
   buttonText: string;
   movieTitle: string;
+  objectPosition?: string;
 }
 
-const MovieCard = ({ imgAlt, imgURL, movieTitle }: MovieCardProps) => {
+const MovieCard = ({
+  imgAlt,
+  imgURL,
+  movieTitle,
+  objectPosition = "center",
+}: MovieCardProps) => {
   return (
     <div>
       <Card isFooterBlurred radius="lg" className="border-none">
@@ -19,6 +25,7 @@ const MovieCard = ({ imgAlt, imgURL, movieTitle }: MovieCardProps) => {
           height={250}
           src={imgURL}
           width={250}
+          style={{ objectPosition: objectPosition }}
         />
         <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
           <p className="text-tiny text-white/80">{movieTitle}</p>
