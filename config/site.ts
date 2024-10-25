33f763a9e32eba0330ca93cee648a8e5034ddf3e
Arnold De La Vega <arnoldd@device-0004.local>
2024-10-24 19:14:52 -0700

    removed favicon svg files and also changed title that shows up in browser tab

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Arnold De La Vega",
+  description: "Systems Administrator",
   navItems: [
     {
       label: "Home",
